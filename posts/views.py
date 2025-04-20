fd914de38431ee0f45824560d440bdf4c797a374
Theo Billardey <toto.billardey@gmail.com>
2025-04-20 17:29:08 +0200

    feat(post): create post details route
    Create a new url in post app and pass context in views for post list to post details navigation

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,26 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import Http404, HttpRequest, HttpResponse
 from django.shortcuts import render
+
+from posts.models import Post
 
 
 def index(request: HttpRequest) -> HttpResponse:
+    try:
+        posts = Post.objects.all()  # TODO: implement proper pagination system
+    except Post.DoesNotExist:
+        raise Http404
+
+    return render(request, "posts/index.html", context={"posts": posts})
+
+
+def post(request: HttpRequest, slug: str) -> HttpResponse:
+    try:
+        post = Post.objects.get(slug=slug)
+    except Post.DoesNotExist:
+        raise Http404
+
     return render(
         request,
-        "posts/index.html",
+        "posts/post.html",
+        context={"title": post.title, "content": post.content},
     )
