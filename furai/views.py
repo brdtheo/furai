0cd51b911ac371a3342b95d367bf11b5f2654243
Theo Billardey <toto.billardey@gmail.com>
2025-04-28 19:15:23 +0200

    feat(templates): create latest post section
    Retrieve latest blog post object and pass it to template context

--- furai/views.py
+++ furai/views.py
@@ -1,12 +1,15 @@
-from datetime import datetime, timezone
-
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from posts.models import Post
+
 
 def index(request: HttpRequest) -> HttpResponse:
+    # Retrieve the latest blog post. If none, hide the section
+    latest_post = Post.objects.latest("created_at")
+
     return render(
         request,
         "furai/index.html",
-        context={"date": datetime.now(timezone.utc)},
+        context={"latest_post": latest_post},
     )
