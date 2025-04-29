fafb421901fcd946957022bcdba1d950d6a60c8b
Theo Billardey <toto.billardey@gmail.com>
2025-04-29 14:35:44 +0200

    feat(templates): create countrydex section

--- furai/views.py
+++ furai/views.py
@@ -7,9 +7,29 @@ from posts.models import Post
 def index(request: HttpRequest) -> HttpResponse:
     # Retrieve the latest blog post. If none, hide the section
     latest_post = Post.objects.latest("created_at")
+    visited_country_list = [
+        "france",
+        "germany",
+        "luxembourg",
+        "spain",
+        "austria",
+        "japan",
+    ]
+    to_visit_country_list = [
+        "thailand",
+        "united_states",
+        "switzerland",
+        "china",
+        "italy",
+        "poland",
+    ]
 
     return render(
         request,
         "furai/index.html",
-        context={"latest_post": latest_post},
+        context={
+            "latest_post": latest_post,
+            "visited_country_list": visited_country_list,
+            "to_visit_country_list": to_visit_country_list,
+        },
     )
