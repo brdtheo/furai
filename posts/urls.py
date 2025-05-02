84e96cb1b60ba03c3c90ae772d0fc07d2f328bf1
Theo Billardey <toto.billardey@gmail.com>
2025-05-02 14:15:49 +0200

    feat(posts): rename blog post routes

--- posts/urls.py
+++ posts/urls.py
@@ -3,6 +3,6 @@ from django.urls import URLPattern, URLResolver, path
 from .views import index, post
 
 urlpatterns: list[URLResolver | URLPattern] = [
-    path("", index, name="post-list"),
-    path("<str:slug>", post, name="post-detail"),
+    path("", index, name="posts"),
+    path("<str:slug>", post, name="post"),
 ]
