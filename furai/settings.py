d4ae4c025398f455f91e155a746cc87736f88a88
Theo Billardey <toto.billardey@gmail.com>
2025-05-07 15:25:53 +0200

    feat(requirements): remove django_browser_reload dependency

--- furai/settings.py
+++ furai/settings.py
@@ -50,7 +50,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "tailwind",
     "theme",
-    "django_browser_reload",
     "posts",
 ]
 
@@ -62,7 +61,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "django_browser_reload.middleware.BrowserReloadMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
