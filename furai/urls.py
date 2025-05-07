6488e0629631f54879a950a1549ef5347bb081a1
Theo Billardey <toto.billardey@gmail.com>
2025-05-07 15:25:53 +0200

    feat(requirements): remove django_browser_reload dependency

--- furai/urls.py
+++ furai/urls.py
@@ -21,7 +21,6 @@ from django.urls import URLPattern, URLResolver, include, path
 from furai.views import index
 
 urlpatterns: list[URLResolver | URLPattern] = [
-    path("__reload__/", include("django_browser_reload.urls")),
     path("admin/", admin.site.urls),
     path("", index, name="home"),
     path("posts/", include("posts.urls")),
