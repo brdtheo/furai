0ce8e100a9fd40e9f9b08fb7b6641a7edee78f2f
Theo Billardey <toto.billardey@gmail.com>
2025-05-09 16:04:52 +0200

    config(settings): add basic logging

--- furai/settings.py
+++ furai/settings.py
@@ -153,3 +153,18 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+
+# Logging
+LOGGING = {  # type: ignore
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING",
+    },
+}
