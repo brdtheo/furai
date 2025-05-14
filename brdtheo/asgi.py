7d3cb4eef358cb641057fdca3153f88bb6e90aa6
Theo Billardey <toto.billardey@gmail.com>
2025-05-14 13:30:36 +0200

    refactor: rename project
    remove all furai occurences

--- brdtheo/asgi.py
+++ brdtheo/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for furai project.
+ASGI config for brdtheo project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "furai.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "brdtheo.settings")
 
 application = get_asgi_application()
