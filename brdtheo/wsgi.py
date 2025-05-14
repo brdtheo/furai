104b744305ae0a8f751e501e4c79d4c5691d06cb
Theo Billardey <toto.billardey@gmail.com>
2025-05-14 13:30:36 +0200

    refactor: rename project
    remove all furai occurences

--- brdtheo/wsgi.py
+++ brdtheo/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for furai project.
+WSGI config for brdtheo project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "furai.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "brdtheo.settings")
 
 application = get_wsgi_application()
