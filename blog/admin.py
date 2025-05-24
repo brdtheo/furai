a2887d34403e9a504a6b6c9bb8767b23fc111394
Theo Billardey <toto.billardey@gmail.com>
2025-05-24 16:15:45 +0200

    feat(blog): register admin models and customize interface

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,28 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, PostCategory, PostThumbnail
 
-admin.site.register(Post)
+
+@admin.register(PostCategory)
+class PostCategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "created_at")
+    list_per_page = 10
+
+
+@admin.register(PostThumbnail)
+class PostThumbnailAdmin(admin.ModelAdmin):
+    list_display = ("url", "created_at")
+    list_per_page = 10
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "thumbnail")
+    list_per_page = 10
+    list_filter = ("categories",)
+    filter_horizontal = ("categories",)
+    list_display_links = (
+        "title",
+        "slug",
+    )
+    search_fields = ("title",)
